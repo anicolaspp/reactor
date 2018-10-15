2ab93f025f619042ca43f893134090ef5992804f
Nicolas A Perez <anicolaspp@gmail.com>
2018-10-14 23:02:51 -0400

    Using the DataSet API
    Using the DataSet API is better for this problem domain than using the RDD API.

--- links_general_stats/build.sbt
+++ links_general_stats/build.sbt
@@ -15,6 +15,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-sql_2.11" % "2.2.1" % "provided",
   "org.apache.spark" % "spark-streaming_2.11" % "2.2.1" % "provided",
   "org.apache.spark" % "spark-streaming-kafka-0-9_2.11" % "2.0.1-mapr-1611",
+  "org.apache.spark" % "spark-sql_2.11" % "2.0.1-mapr-1707",
   "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7",
   "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.9.7",
   "com.typesafe.play" % "play-json_2.11" % "2.3.8",
--- links_general_stats/src/main/scala/com/github/anicolaspp/generalstats/App.scala
+++ links_general_stats/src/main/scala/com/github/anicolaspp/generalstats/App.scala
@@ -1,18 +1,14 @@
 package com.github.anicolaspp.generalstats
 
 import com.github.anicolaspp.generalstats.data.Streamer
-import com.mapr.db.MapRDB
-import com.mapr.db.spark.MapRDBSpark
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
-import org.apache.spark.streaming.dstream.InputDStream
-import org.apache.spark.streaming.kafka09.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import com.mapr.db.spark.{MapRDBSpark, _}
+import org.apache.spark.sql._
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 import org.apache.spark.streaming.{Milliseconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
-import com.mapr.db.spark._
-import com.mapr.db.spark.impl.OJAIDocument
-import org.ojai.scala.Document
 
-import scala.util.Try
+import org.apache.spark.sql.SparkSession
+
 
 object App extends Streamer {
 
@@ -24,42 +20,93 @@ object App extends Streamer {
 
     val messages = getStream("/user/mapr/streams/click_stream:all_links_2")
 
+    val sparkSession = SparkSession.builder().getOrCreate()
+
     messages
       .map(_.value())
       .map(MapRDBSpark.newDocument)
       .map(doc => (doc.getString("path"), 1))
       .reduceByKey(_ + _)
+
       .foreachRDD { rdd =>
+        val m = rdd.map { case (path, sum) => Row(path, sum) }
 
-        val fromDB = sc
+        val df = sparkSession.createDataFrame(m, StructType(List(StructField("_id", DataTypes.StringType), StructField("sum", DataTypes.IntegerType))))
+
+        println("stream")
+        df.show()
+
+        val fromDBRDD = sc
           .loadFromMapRDB("/user/mapr/tables/total_counts")
-          .map(d => (d.getString("_id"), d))
+          .map(d => (d.getString("_id"), d.getInt("total")))
+          .map { case (path, sum) => Row(path, sum) }
 
-        val joint = rdd.join(fromDB)
-
-        joint
-          .map { jointPair =>
-            val docTotal = Try {
-              jointPair._2._2.getString("total").toInt
-            }.getOrElse(0)
-
-            val add = jointPair._2._1
-
-            jointPair._2._2.set("total", (docTotal + add).toString)
-          }
-          .saveToMapRDB("/user/mapr/tables/total_counts")
+        val fromDBDF = sparkSession.createDataFrame(fromDBRDD, StructType(List(StructField("_id", DataTypes.StringType), StructField("total", DataTypes.IntegerType))))
+          
 
 
-        val paths = joint.map(_._1).collect().toSet
+        val join = df.join(fromDBDF, Seq("_id"), "left_outer").na.fill(0, Seq("total"))
 
-        rdd
-          .filter(p => !paths.contains(p._1))
-          .map(p => MapRDB.newDocument().set("_id", p._1).set("total", p._2))
-          .saveToMapRDB("/user/mapr/tables/total_counts")
+//        join.show()
+
+        val finalDF = join.withColumn("value", join("sum") + join("total"))
+
+        println("final")
+        finalDF.show()
+
+        finalDF.sa
       }
+    //      .map { case (path, sum) =>
+    //
+    //        val fromDB = sc
+    //          .loadFromMapRDB("/user/mapr/tables/total_counts")
+    //          .map(d => (d.getString("_id"), d))
+    //          .filter(_._1 == path)
+    //
+    //        val valueOnDB = fromDB.map { case (_, doc) =>
+    //          Try {
+    //            doc.getString("total").toInt
+    //          }.getOrElse(0)
+    //        }.reduce(_ + _)
+    //
+    //        (path, sum + valueOnDB)
+    //      }
+    //      .map { case (path, finalSum) => MapRDB.newDocument().set("_id", path).set("total", finalSum) }
+    //      .foreachRDD(_.saveToMapRDB("/user/mapr/tables/total_counts"))
+
+
+    //      .foreachRDD { rdd =>
+    //
+    //        val fromDB = sc
+    //          .loadFromMapRDB("/user/mapr/tables/total_counts")
+    //          .map(d => (d.getString("_id"), d))
+    //
+    //        val joint = rdd.join(fromDB)
+    //
+    //        joint
+    //          .map { jointPair =>
+    //            val docTotal = Try {
+    //              jointPair._2._2.getString("total").toInt
+    //            }.getOrElse(0)
+    //
+    //            val add = jointPair._2._1
+    //
+    //            jointPair._2._2.set("total", (docTotal + add).toString)
+    //          }
+    //          .saveToMapRDB("/user/mapr/tables/total_counts")
+    //
+    //
+    //        val paths = joint.map(_._1).collect().toSet
+    //
+    //        rdd
+    //          .filter(p => !paths.contains(p._1))
+    //          .map(p => MapRDB.newDocument().set("_id", p._1).set("total", p._2))
+    //          .saveToMapRDB("/user/mapr/tables/total_counts")
+    //      }
 
     ssc.start()
     ssc.awaitTermination()
   }
+
 }
 

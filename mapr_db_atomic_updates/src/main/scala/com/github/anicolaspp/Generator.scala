7a68bcd29f8f32e2b02f374c0fb547af8d7e9391
Nicolas A Perez <anicolaspp@gmail.com>
2018-12-01 23:13:59 -0500

    random data generator

---
+++ mapr_db_atomic_updates/src/main/scala/com/github/anicolaspp/Generator.scala
@@ -0,0 +1,32 @@
+package com.github.anicolaspp
+
+import com.mapr.db.MapRDB
+import org.ojai.store.DocumentStore
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Random
+
+object Generator {
+
+  def generate(numberOfSessions: Int)(implicit ex: ExecutionContext, store: DocumentStore): Future[List[String]] = Future {
+    val inserts = (1 to numberOfSessions)
+      .par
+      .map(_ => Random.nextString(1000))
+      .map { randomSessionId =>
+        Future {
+          val document = MapRDB.newDocument()
+            .set("_id", randomSessionId)
+            .set("count", 0)
+
+          store.insert(document)
+
+          randomSessionId
+        }
+      }
+        .toStream
+
+    Future.foldLeft(inserts)(List.empty[String])((xs, id) => id :: xs)
+
+  }.flatten
+
+}
